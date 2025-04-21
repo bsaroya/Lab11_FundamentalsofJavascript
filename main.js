fccd0f9caf943e6d585ce2590cbe160e80f5894c
bsaroya <balrajssaroya@gmail.com>
2025-04-21 13:56:33 -0400

    Create Product class with constructor
    Create Product class with constructor

---
+++ main.js
@@ -0,0 +1,8 @@
+// Product class handles basic product information and stock value
+class Product {
+  constructor(name, price, quantity) {
+    this.name = name;         // Name of the product 
+    this.price = price;       // Price per unit
+    this.quantity = quantity; // Units available in stock
+  }
+}
